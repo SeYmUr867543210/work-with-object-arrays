b2e997d5f7c00b2684e8ba4fb9f79f202004b447
SeYmUr867543210 <64531437+SeYmUr867543210@users.noreply.github.com>
2021-10-07 21:12:02 +0500

    requested changes

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 // Напиши функцию, генерирующую объект со случайным именем, случайной фамилией и случайным возрастом. Имена и фамилии должны браться из массивов имён и фамилий соответственно. Возраст должен генерироваться случайным целым числом.
 
-let arrOfNames = ["Misha", "Grisha", "Vanya", "Sam", "Anton"];
-let arrOfSirnames = ["Mamedov", "Vasilkov", "Ninin", "Voronov", "Minin"];
-let personsAmount = arrOfSirnames.length > arrOfSirnames.length ? arrOfSirnames.length : arrOfSirnames.length;
-let arrOfAges = [];
-for (let i = 0; i < personsAmount; i++) {
-    arrOfAges.push(parseInt(Math.random() * 100));
-}
+const arrOfNames = ["Misha", "Grisha", "Vanya", "Sam", "Anton"];
+const arrOfSirnames = ["Mamedov", "Vasilkov", "Ninin", "Voronov", "Minin"];
+const personsAmount = arrOfSirnames.length > arrOfSirnames.length ? arrOfSirnames.length : arrOfSirnames.length;
 
 let randomPersons = [];
 
 function generateRandomPerson() {
     for (let i = 0; i < personsAmount; i++) {
+        let age = getRandomNum();
         let randomIdx = parseInt(Math.random() * personsAmount);
 
-        let randomNameFromArrOfNames = arrOfNames[randomIdx];
-        let randomSirnameFromArrOfSirnames = arrOfSirnames[randomIdx];
-        let randomAgeFromArrOfAges = arrOfAges[randomIdx];
+        let firstname = arrOfNames[randomIdx];
+        let lastname = arrOfSirnames[randomIdx];
 
-        randomPersons.push({ name: randomNameFromArrOfNames, sirname: randomSirnameFromArrOfSirnames, age: randomAgeFromArrOfAges })
+        randomPersons.push({ firstname, lastname, age })
     }
 
 }
 generateRandomPerson();
+
+function getRandomNum() {
+    return parseInt(Math.random() * 100);
+}
